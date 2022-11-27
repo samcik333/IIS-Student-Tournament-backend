355839fa8bb36651de02cd8e06fe679fd9c0b27f
Samueel12 <samuel.sulo@gmail.com>
2022-11-27 12:17:08 +0100

    team players

--- src/controllers/team.ts
+++ src/controllers/team.ts
@@ -1,11 +1,16 @@
 import { Express, Response, Request } from "express";
-import { saveTeam } from "../services/teamService/create";
-import { deleteTeamByName } from "../services/teamService/delete";
+import { saveMember, saveTeam } from "../services/teamService/create";
+import { deleteMember, deleteTeamByName } from "../services/teamService/delete";
 import {
+	getMemberByUsername,
+	getOwnerByTeamId,
+	getPlayersByTeamId,
 	getTeamById,
 	getTeamByName,
 	getTeams,
 	getTeamsByUserId,
+	getUserById,
+	getUserByUsername,
 } from "../services/teamService/read";
 import { updateTeamById } from "../services/teamService/update";
 
@@ -32,6 +37,15 @@ export const teamInfo = async (req: Request, res: Response) => {
 		return res.status(409).json({ message: "Team does not exist" });
 	}
 	return res.status(200).send(team);
+};
+
+/* TEAM PLAYER LIST */
+export const teamPlayerList = async (req: Request, res: Response) => {
+	const playerList = await getPlayersByTeamId(req);
+	if (!playerList) {
+		return res.status(409).json({ message: "There are no players here" });
+	}
+	return res.status(200).send(playerList);
 };
 
 /* CREATE TEAM */
@@ -81,4 +95,54 @@ export const deleteTeam = async (req: Request, res: Response) => {
 	return res
 		.status(200)
 		.json({ message: "The team " + req.body.name + " was deleted" });
+};
+
+/* ADD PLAYER */
+export const teamAddPlayer = async (req: Request, res: Response) => {
+	const userToAdd = await getUserByUsername(req);
+	if (!userToAdd) {
+		return res.status(400).json({ message: "User was not found" });
+	}
+
+	const isMember = await getMemberByUsername(req);
+	if (isMember) {
+		return res.status(400).json({ message: "User is already member of team" });
+	}
+
+	const newMember = await saveMember(req);
+	if (!newMember) {
+		return res.status(409).json({ message: "User was not added" });
+	}
+	return res.status(200).json({ message: "User was added" });
+};
+
+/* DELETE PLAYER */
+export const deletePlayer = async (req: Request, res: Response) => {
+	const delPlayer = await deleteMember(req);
+	if (!delPlayer) {
+		return res.status(409).json({
+			message: "User does not exist",
+		});
+	}
+	return res
+		.status(200)
+		.json({ message: "User " + req.body.username + " was deleted" });
+};
+
+/* PLAYER INFO */
+export const playerInfo = async (req: Request, res: Response) => {
+	const user = await getUserById(req);
+	if (!user) {
+		return res.status(409).json({ message: "Player does not exist" });
+	}
+	return res.status(200).send(user);
+};
+
+/* TEAM OWNER */
+export const teamOwner = async (req: Request, res: Response) => {
+	const owner = await getOwnerByTeamId(req);
+	if (!owner) {
+		return res.status(409).json({ message: "Owner does not exist" });
+	}
+	return res.status(200).send(owner);
 };
