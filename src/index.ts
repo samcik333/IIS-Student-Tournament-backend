c8e3279992a5f878958f71d8ab471a089d2cdd07
Samuel Sadlek <samcik333@gmail.com>
2022-11-30 23:19:36 +0100

    index

--- src/index.ts
+++ src/index.ts
@@ -25,16 +25,20 @@ var distDir = __dirname + "/dist/";
 
 app.set("trust proxy", 1); // trust first proxy
 app.use(function (req, res, next) {
-	res.header("Access-Control-Allow-Origin", url); // update to match the domain you will make the request from
+	res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
 	res.header(
 		"Access-Control-Allow-Headers",
-		"Origin, X-Requested-With, Content-Type, Accept,Authorization"
+		"Origin, X-Requested-With, Content-Type, Accept Authorization"
 	);
-	res.header(
-		"Access-Control-Allow-Methods",
-		"GET, POST, OPTIONS, PUT, PATCH, DELETE"
-	);
-	res.header("Access-Control-Allow-Credentials", "true");
+	res.header("Access-Control-Allow-Credentials", "false");
+	if (req.method === "OPTIONS") {
+		res.header(
+			"Access-Control-Allow-Methods",
+			"POST, PUT, PATCH, GET, DELETE"
+		);
+		return res.status(200).json({});
+	}
+
 	next();
 });
 var corsOptions = {
