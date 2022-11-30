365c192f05030fea2749e9c72c3559dc54272160
Samuel Sadlek <samcik333@gmail.com>
2022-11-30 22:43:26 +0100

    index

--- src/index.ts
+++ src/index.ts
@@ -28,7 +28,7 @@ app.use(function (req, res, next) {
 	res.header("Access-Control-Allow-Origin", url); // update to match the domain you will make the request from
 	res.header(
 		"Access-Control-Allow-Headers",
-		"Origin, X-Requested-With, Content-Type, Accept"
+		"Origin, X-Requested-With, Content-Type, Accept,Authorization"
 	);
 	res.header(
 		"Access-Control-Allow-Methods",
