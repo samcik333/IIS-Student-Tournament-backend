0dfd36b4c329f8c56825ac96c986fd6834b07c47
xxGEORGEx <jurajbagi@hotmail.sk>
2022-12-01 00:40:45 +0100

    index

--- src/index.ts
+++ src/index.ts
@@ -25,12 +25,12 @@ var distDir = __dirname + "/dist/";
 
 app.set("trust proxy", 1); // trust first proxy
 app.use(function (req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+	res.header("Access-Control-Allow-Origin", url); // update to match the domain you will make the request from
 	res.header(
 		"Access-Control-Allow-Headers",
 		"Origin, X-Requested-With, Content-Type, Accept Authorization"
 	);
-	res.header("Access-Control-Allow-Credentials", "false");
+	res.header("Access-Control-Allow-Credentials", "true");
 	if (req.method === "OPTIONS") {
 		res.header(
 			"Access-Control-Allow-Methods",
