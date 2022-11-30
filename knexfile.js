120d1d734f9233a424453ba5be9e461bb64bd37b
Samuel Sadlek <samcik333@gmail.com>
2022-11-30 22:55:41 +0100

    knex

--- knexfile.js
+++ knexfile.js
@@ -25,7 +25,7 @@ module.exports = {
 	production: {
 		client: "pg",
 		connection: process.env.DATABASE_URL,
-		ssl: true,
+		ssl: false,
 		migrations: {
 			directory: "./src/migrations/prod",
 		},
