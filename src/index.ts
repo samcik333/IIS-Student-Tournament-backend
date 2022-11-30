d4e4a02bc5bcbb3b74fb6f0583e40f6cc7187561
xxGEORGEx <jurajbagi@hotmail.sk>
2022-11-30 21:23:50 +0100

    access allow

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import cors from "cors";
 
 var knexConfiig = require("../knexfile");
 let knex;
-let url;
+let url: string | string[];
 if (process.env.NODE_ENV == "production") {
 	knex = Knex(knexConfiig.production);
 	url = "https://sjsquad.herokuapp.com";
@@ -17,13 +17,29 @@ if (process.env.NODE_ENV == "production") {
 	url = "http://localhost:4200";
 }
 
+
+
 Model.knex(knex);
 
 const PORT = process.env.PORT || 5005;
 const app = express();
 var distDir = __dirname + "/dist/";
 
-app.use(
+app.set('trust proxy', 1) // trust first proxy
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+	res.header('Access-Control-Allow-Credentials', "true");
+	next();
+});
+var corsOptions = {
+	origin: url,
+	optionsSuccessStatus: 200 // some legacy browsers, choke on 204 
+};
+app.use(cors(corsOptions));
+
+/*app.use(
 	cors({
 		origin: url,
 		allowedHeaders: [
@@ -34,7 +50,8 @@ app.use(
 		],
 		credentials: true,
 	})
-);
+);*/
+
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(cookieParser());
